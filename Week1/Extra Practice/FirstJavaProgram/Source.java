615655aa3ec73a975775d5da9a1541fe3365e6a0
swright96 <swright96@live.com>
2022-04-04 01:21:59 -0600

    initial commit

---
+++ Week1/Extra Practice/FirstJavaProgram/Source.java
@@ -0,0 +1,7 @@
+public class Source {
+    public static void main(String[] args){
+        System.out.println("My name is Samuel");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Sterling, CO");
+    }
+}
