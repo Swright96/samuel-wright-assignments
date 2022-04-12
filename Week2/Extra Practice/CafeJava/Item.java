8b7c773e24cba3f52b15d9a78e410f32954db8e7
swright96 <swright96@live.com>
2022-04-12 10:35:48 -0600

    updated Item and Order

--- Week2/Extra Practice/CafeJava/Item.java
+++ Week2/Extra Practice/CafeJava/Item.java
@@ -1,4 +1,27 @@
-class Item {
-    public String name;
-    public double price;
+public class Item {
+    private String name;
+    private double price;
+    
+    //Item Constructor
+    public Item (String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    //Item name getter
+    public String getItemName() {
+        return name;
+    }
+    //Item name setter
+    public void setItemName(String name) {
+        this.name = name;
+    }
+
+    //Item price getter
+    public double getItemPrice() {
+        return price;
+    }
+    //Item price setter
+    public void setItemPrice(double price){
+        this.price = price;
+    }
 }
--- Week2/Extra Practice/CafeJava/Order.java
+++ Week2/Extra Practice/CafeJava/Order.java
@@ -1,8 +1,65 @@
 import java.util.ArrayList;
 
-class Order {
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Item> items;
+public class Order {
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items;
+
+    //default Order constructor
+    public Order (){
+        this.name = "Guest";
+        ArrayList<Item> items = new ArrayList<Item>();
+    }
+    // Overloaded Order constructor
+    public Order (String name) {
+        this.name = name;
+        this.items = new ArrayList<Item>();
+    }
+
+    //Gettors and Setters
+    public String getOrderName(){
+        return name;
+    }
+    public void setOrderName(String name){
+        this.name = name;
+    }
+    public boolean getReadyStatus(){
+        return ready;
+    }
+    public void setReadyStatus(boolean ready){
+        this.ready = ready;
+    }
+    public ArrayList<Item> getItems(){
+        return items;
+    }
+    public void setItems(ArrayList<Item> items){
+        this.items = items;
+    }
+
+    //Methods
+    public void addItem(Item Item){
+        this.items.add(Item);
+    }
+    public String getStatusMessage(){
+        if (this.ready == true){
+            return "Your order is ready.";
+        }
+        else {
+            return "Thank you for waiting, your order will be ready shortly.";
+        }
+    }
+    public double getOrderTotal(){
+        double sum = 0.0;
+        for (Item i: this.items){       //previously for (int i = 0; i < this.items.size(); i++)
+            sum += i.getItemPrice();    //would return error int cannot be dereferenced
+        }
+        return sum;
+    }
+    public void display(){
+        System.out.printf("Customer Name: %s", this.getOrderName());
+        for (int i = 0; i < this.items.size(); i++){
+            System.out.printf("%s - %s \n", this.items.get(i).getItemName(), this.items.get(i).getItemPrice());
+        }
+        System.out.println(getOrderTotal());
+    }
 }
